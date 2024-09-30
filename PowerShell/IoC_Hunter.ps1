7d24c583abc5377a9e256ebc36287cd758b9f0f2
Foxtrot-813 <barzani.j@gmail.com>
2024-09-30 10:54:43 +0200

    Check parameters

--- PowerShell/IoC_Hunter.ps1
+++ PowerShell/IoC_Hunter.ps1
@@ -1,19 +1,20 @@
 $hostname = hostname
 $date = Get-Date -Format "yyyy-MM-dd-HH:mm"
-$working_path = "C:\Users\barjaw\Downloads\IoC_Working_Path\opt\security\oporation"
+$working_path = "C:\Users\barjaw\Downloads\IoC_Working_Path\opt\security\oporation\"
 $errors_path = "C:\Users\barjaw\Downloads\IoC_Working_Path\opt\security\errors"
-$datestamp = Get-Date -Format "yyyyMMdd"
+# $datestamp = Get-Date -Format "yyyyMMdd"
+$datestamp ="20231122"
 $server_url = $args[0]
 $upload_server = $args[1]
 $user_identity = $args[2]
 $flag1 = "0"
-$flag2 = "="
+$flag2 = "0"
 
 # Log files
 $ioc_file = "IOC-$datestamp.ioc"
-$ioc_gpg = 
-$ioc_log = 
-$error_file = 
+$ioc_gpg = "IOC-${datestamp}.gpg"
+$ioc_log = "IOC-${datestamp}.log"
+$error_file = "error-${datestamp}.log"
 
 Write-Host "CND-Hunter by 8$([char]0x039B)$([char]0x042F)24$([char]0x0418)1"
 Write-Host "TTH IoC Check for $hostname $date STARTED"
@@ -21,7 +22,9 @@ Write-Host "======================= IOC Hunter ======================="
 
 # Check parameters
 Function Initiate_Check {
-    if ($server_url -Match "^http://") {
+    if (-Not $server_url) {
+        Write-Host "No URL provided."
+    } elseif ($server_url -Match "^http://") {
         Write-Host "WARNING: The URL uses HTTP. Only HTTPS is supported for secure communication."
     } elseif ($server_url -Match "^https://") {
         Write-Host "URL validation successful: HTTPS protocol detected."
@@ -40,10 +43,72 @@ Function Initiate_Check {
     } else {
         Write-Host "Directory already exists at: $working_path. No action needed."
     }
+    Set-Location $working_path
+    "Current working directory is: " + (Get-Location).Path
+}
+
+# Restore system environment
+Function restore_system_env {
+    Write-Host "Restoring the previous system environment...."
+    if ($flag1 -eq "0") {
+        if (Test-Path "$working_path$ioc_file") {
+            Remove-Item "$working_path$ioc_file" -Force
+            Write-Host "File $ioc_file in $working_path deleted successfully."
+        }
     
+        if (Test-Path "$working_path$ioc_gpg") {
+            Remove-Item "$working_path$ioc_gpg" -Force
+            Write-Host "File $ioc_gpg in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path$ioc_log") {
+            Remove-Item "$working_path$ioc_log" -Force
+            Write-Host "File $ioc_log in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path\binfailure") {
+            Remove-Item "$working_path\binfailure" -Force
+            Write-Host "File binfailure in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path\listeningports") {
+            Remove-Item "$working_path\listeningports" -Force
+            Write-Host "File listeningports in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path\firewall") {
+            Remove-Item "$working_path\firewall" -Force
+            Write-Host "File firewall in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path\$hostname-tth-$datestamp.tgz") {
+            Remove-Item "$working_path\$hostname-tth-$datestamp.tgz" -Force
+            Write-Host "File $hostname-tth-$datestamp.tgz in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path\$hostname-tth-$datestamp.tgz.sig") {
+            Remove-Item "$working_path\$hostname-tth-$datestamp.tgz.sig" -Force
+            Write-Host "File $hostname-tth-$datestamp.tgz.sig in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path\iocreport-$datestamp.txt") {
+            Remove-Item "$working_path\iocreport-$datestamp.txt" -Force
+            Write-Host "File iocreport-$datestamp.txt in $working_path deleted successfully."
+        }
+    
+        if (Test-Path "$working_path\msg") {
+            Remove-Item "$working_path\msg" -Force
+            Write-Host "File msg in $working_path deleted successfully."
+        }
+    } elseif ($flag1 -eq "1") {
+        Remove-Item "/opt/security/working" -Recurse -Force
+        Write-Host "/opt/security/working directory deleted successfully."
+    }
+    
+
+
 }
 
 
-
-
 Initiate_Check
+restore_system_env
